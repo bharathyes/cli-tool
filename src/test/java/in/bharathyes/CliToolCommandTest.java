509157094efe6761e7f398305026d6bb6f8e9352
Bharath S <bharathyes>
2024-01-27 13:46:18 +0000

    Initial commit

---
+++ src/test/java/in/bharathyes/CliToolCommandTest.java
@@ -0,0 +1,28 @@
+package in.bharathyes;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CliToolCommandTest {
+
+    @Test
+    public void testWithCommandLineOption() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+            String[] args = new String[] { "-v" };
+            PicocliRunner.run(CliToolCommand.class, ctx, args);
+
+            // cli-tool
+            assertTrue(baos.toString().contains("Hi!"));
+        }
+    }
+}
